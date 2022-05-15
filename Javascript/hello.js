9264886973d9bc04f2361a3d643461248c127510
Jayyahoo <wachirawit050116@gmail.com>
2022-05-15 17:39:45 +0700

    wachirawit

---
+++ Javascript/hello.js
@@ -0,0 +1,62 @@
+// alert("Hello");
+
+// var FirstName = "Wachirawit";
+// var LastName = "Mool-ang";
+// var age = 20;
+// console.log(typeof age);
+// console.log(typeof false);
+// console.log(typeof FirstName );
+
+// console.log(FirstName + LastName);
+
+// console.log(FirstName - LastName);
+// NaN: Not a Number ไม่ใช่ค่าตัวเลข
+
+// {
+//     var FirstName = "wachirawit"; 
+//     // console.log(FirstName);
+// }
+
+// alert(FirstName);66
+//  var  จะไม่มี Scope หรือเวลาประกาศตัวแปร ถ้าจะเรียกใช้ก็สามารถเรียกใช้ได้เลยโดยไม่ต้องทำงานภายใน {}
+
+// /............................................../ 
+// {
+//     let LastName = "mool-ang" 
+// }
+// alert(LastName);
+// let มี Scope ในการทำงาน หรือเวลาประกาศตัวแปรทำงานภายใน {}
+
+// let name = prompt("Please enter a message..");
+// console.log(name);
+
+// let lotto_number = prompt("Please enter a number");
+// console.log(lotto_number);
+// console.log(Math.floor(Math.random() * 100));
+
+// let lotto_number = prompt("กรอกเลขที่ต้องการซื้อ");
+// document.getElementById("value").innerHTML = lotto_number;
+// console.log(lotto_number);
+
+// document.getElementById("result").innerHTML = Math.floor(Math.random() * 100)
+
+console.log(10 == 10);
+console.log(10 == 20);
+
+console.log("10" == 10);
+// ในภาษา Javascript == เป็นการเปรียบเทียบค่า ฝั่งซ้าย เท่ากับ ฝั่งขวา โดยจะไม่สนชนิดของข้อมูล
+
+console.log("10" === 10);
+// ในภาษา Javascript === เป็นการเปรียบเทียบค่า ฝั่งซ้าย เท่ากับ ฝั่งขวา และชนิดของข้อมูล
+
+console.log("hello" == "Hello");
+// ในภาษา Javascript ตัวพิมพ์เล็กพิมใหญ่ คือคนละตัวกัน
+
+console.log("5" != "5");
+// != คือเครื่องหมาย ไม่เท่ากับ
+
+console.log("hello" != "Hello");
+console.log("4" != 4);
+
+console.log("5" !== 5);
+// !== เป็นการเปรียบและ ชนิดของข้อมูล
