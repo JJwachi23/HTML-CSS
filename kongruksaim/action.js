f7c3ee91b0454cde8fb179efabd60b21afeee13c
Jayyahoo <wachirawit050116@gmail.com>
2022-07-24 21:03:03 +0700

    wachirwait

--- kongruksaim/action.js
+++ kongruksaim/action.js
@@ -1,1 +1,5 @@
-document.write("<h2>Hello World!</h2>");
+document.write("<h2>Hello World!</h2>");    //แสดงเป็นประเภท string หรือ HTML 
+
+alert("Hello Wachirawit!"); // เป็นการแจ้งเตือนผู้ใช้งานเว็บ
+
+console.log("wachirawit mool-ang"); // เป็นการพิมพ์ข้อความหรือตัวแปร ใช้ debug ค่าต่างๆ 
