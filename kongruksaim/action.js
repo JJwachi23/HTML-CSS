590f3eb7db1b8be3d350e96c597ba5f58277cba1
Jayyahoo <wachirawit050116@gmail.com>
2022-07-25 14:54:16 +0700

    wachirawit

--- kongruksaim/action.js
+++ kongruksaim/action.js
@@ -1,13 +1,40 @@
-document.write("<h2>Hello World!</h2>");    //แสดงเป็นประเภท string หรือ HTML 
+/* document.write("<h2>Hello World!</h2>");    //แสดงเป็นประเภท string หรือ HTML 
 
 alert("Hello Wachirawit!"); // เป็นการแจ้งเตือนผู้ใช้งานเว็บ
 
 console.log("wachirawit mool-ang"); // เป็นการพิมพ์ข้อความหรือตัวแปร ใช้ debug ค่าต่างๆ 
 
 let name //สร้างตัวแปรแต่ไม่มีการกำหนดค่า
-name = "wachirawit" //กำหรดข้อความลงไปในตัวแปร
-let age = 20;
-const LastName = "Mool-ang"
+name = "wachirawit" //กำหรดข้อความลงไปในตัวแปร 
 
-console.log(name);
-console.log("อายุ = ", age);
+let _for = "JavaScript", age = 20, Name = "wachirawit";
+
+console.log(_for);
+console.log(age);
+console.log(Name);
+
+console.log("อายุ = ", age);*/
+
+/*let _for = "Jele", age = 20, Name = "Wachirawit";
+console.log("Before", _for);
+
+_for = "สอนเขียนเว็บ";
+console.log("After", _for); // let สามารถเปลี่ยนค่าตัวแปรได้ */
+
+/* const Name = "wachirawit"   // const: constant หมายถึง ค่าคงที่
+console.log(Name);
+
+Name = "jele"
+console.log(Name);  */
+
+let FirstName = "wachirawit"
+let LastName = "Mool-ang"
+let age = 20;
+let price = 199.99;
+let massage = "Hello!"
+
+console.log(typeof (FirstName));
+console.log(LastName);
+console.log(age);
+console.log(typeof(price));
+console.log(massage);
