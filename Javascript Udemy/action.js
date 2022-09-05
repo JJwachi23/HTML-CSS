f158bedf5a9019312242267bad177700819a7b43
Jayyahoo <wachirawit050116@gmail.com>
2022-09-05 20:50:37 +0700

    wachirawit

--- Javascript Udemy/action.js
+++ Javascript Udemy/action.js
@@ -1,4 +1,4 @@
-var b = 0;
+/* var b = 0;
 var c = add(50, 100);
 function message(a){
     var b = 0;
@@ -10,4 +10,23 @@ function message(a){
 
 function add (a, b){
     return a + b;
-} 
+} */
+
+var person = {};
+person.first = "Laurence";
+person.last = "Svekis";
+person.message = function(){
+    return 'Hello' + this.first;
+}
+
+person.age = 30;
+person.alive = true;
+
+function fun1(){
+    console.log('fun1');
+}
+
+var fun2 = function (){
+    console.log('fun2');
+}
+
