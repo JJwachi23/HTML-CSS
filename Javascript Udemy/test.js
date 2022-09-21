7ed16efb1fd66c1dae3687bc55d9a3979e6e5104
Jayyahoo <wachirawit050116@gmail.com>
2022-09-21 12:30:15 +0700

    wachirawit

--- Javascript Udemy/test.js
+++ Javascript Udemy/test.js
@@ -2,6 +2,7 @@
 //     console.log("Hello JavaScript!");
 // }
 
+
 var car = {
     color: 'red',
     year: 2022,
@@ -10,3 +11,17 @@ var car = {
     price: 50000
 };
 
+//document.querySelector('h2').innerHTML = car.color + ' ' + car.make;
+
+/*var myArray1 = ['wachirawit','Mool-ang','20','false']; //Array คือการเก็บหลายค่าในตัวแปรเดียว 
+myArray1[myArray1.length] = "Last"; //(ชื่อ Array [myA])
+myArray1[2] = 40;
+
+const weeks = ['Sunday','Monday','Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+*/
+
+var myArray1 = ['wachirawit', 'mool-ang', 20, true];
+var varl1 = myArray1.push('test');
+
+console.log(myArray1);
+console.log(varl1);
